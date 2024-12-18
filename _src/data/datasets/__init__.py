1ea67975afab39ecf74196db5d97b314b3da0c44
smkyrle <smoneykyrle@gmail.com>
2024-12-18 13:53:35 +0000

    dataset imports

---
+++ _src/data/datasets/__init__.py
@@ -0,0 +1,8 @@
+from .base import BaseDataset
+from .qmugs import QMugs
+from .biogen import (
+    Biogen, Efflux, HClint,
+    HPPB, RClint, RPPB,
+    Solu,
+)
+from .molecule_net import MoleculeNet
