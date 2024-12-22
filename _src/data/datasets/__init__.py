3c6fe46d07e6297216e2445425bdb13435b910c7
smkyrle <smoneykyrle@gmail.com>
2024-12-22 15:41:25 +0000

    import dataset classes

--- _src/data/datasets/__init__.py
+++ _src/data/datasets/__init__.py
@@ -1,8 +1,14 @@
 from .base import BaseDataset
-from .qmugs import QMugs
 from .biogen import (
     Biogen, Efflux, HClint,
     HPPB, RClint, RPPB,
     Solu,
 )
-from .molecule_net import MoleculeNet
+from .chembl_affinity import DRD2, FactorXA
+from .molecule_net import (
+    BACE, BBBP, ClinTox,
+    ESOL, FreeSolv, HIV,
+    Lipo, MoleculeNet, MUV,
+    SIDER, Tox21, ToxCast,
+)
+from .qmugs import QMugs
