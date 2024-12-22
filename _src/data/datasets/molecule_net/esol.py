36173640f98afbde8bb9bcf8064a57d5f1299cf9
smkyrle <smoneykyrle@gmail.com>
2024-12-22 15:42:49 +0000

    change column names

--- _src/data/datasets/molecule_net/esol.py
+++ _src/data/datasets/molecule_net/esol.py
@@ -1,7 +1,22 @@
 from ..base import BaseDataset
+import numpy as np
 
 class ESOL(BaseDataset):
     url = 'https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/delaney-processed.csv'
 
     def __init__(self, csv: str|None = None):
         super(ESOL, self).__init__(csv=csv, url=self.url)
+        self.rename(
+            columns={
+                'smiles': 'SMILES',
+                'measured log solubility in mols per litre': 'y'
+            },
+            inplace=True,
+        )
+        # unit conversion from mol/L to uM
+        self['y'] = self['y'].apply(lambda x: x + 6)
+        self.drop(
+            self.columns.difference(['SMILES', 'y']),
+            axis=1, inplace=True
+        )
+        self.save()
