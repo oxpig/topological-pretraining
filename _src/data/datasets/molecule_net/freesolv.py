ecdaae4ef9abd34568f60c645195dfcff34e3ff0
smkyrle <smoneykyrle@gmail.com>
2024-12-24 20:36:45 +0000

    compress file and rename col

--- _src/data/datasets/molecule_net/freesolv.py
+++ _src/data/datasets/molecule_net/freesolv.py
@@ -1,7 +1,19 @@
 from ..base import BaseDataset
+from pathlib import Path
 
 class FreeSolv(BaseDataset):
     url = 'https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/SAMPL.csv'
 
-    def __init__(self, csv: str|None = None):
-        super(FreeSolv, self).__init__(csv=csv, url=self.url)
+    def __init__(self, root: str|None = None, compression: bool = True):
+        suffix = 'csv' if compression else 'csv.gz'
+        csv = Path(root) / f'freesolv.{suffix}' if root else None
+        super(FreeSolv, self).__init__(csv=csv, url=self.url, compression=compression)
+        self.rename(
+            columns={'smiles': 'SMILES', 'expt': 'y'},
+            inplace=True
+        )
+        self.drop(
+            self.columns.difference(['SMILES', 'y', 'calc']),
+            axis=1, inplace=True
+        )
+        self.save()
