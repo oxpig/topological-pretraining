9a614ff661ce59e86c15cefbca877a6a345b2251
smkyrle <smoneykyrle@gmail.com>
2024-12-24 20:38:29 +0000

    csv compression

--- _src/data/datasets/molecule_net/tox21.py
+++ _src/data/datasets/molecule_net/tox21.py
@@ -1,7 +1,10 @@
 from ..base import BaseDataset
+from pathlib import Path
 
 class Tox21(BaseDataset):
     url = 'https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/tox21.csv.gz'
 
-    def __init__(self, csv: str|None = None):
-        super(Tox21, self).__init__(csv=csv, url=self.url)
+    def __init__(self, root: str|None = None, compression: bool = True):
+        suffix = 'csv' if compression else 'csv.gz'
+        csv = Path(root) / f'tox21.{suffix}' if root else None
+        super(Tox21, self).__init__(csv=csv, url=self.url, compression=compression)
