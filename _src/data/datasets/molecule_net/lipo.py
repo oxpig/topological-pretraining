3336e0e2a8df70016d0eb385d399b2e8ac1acd9d
smkyrle <smoneykyrle@gmail.com>
2024-12-24 20:37:10 +0000

    csv compression and rename col

--- _src/data/datasets/molecule_net/lipo.py
+++ _src/data/datasets/molecule_net/lipo.py
@@ -1,7 +1,18 @@
 from ..base import BaseDataset
 
+from pathlib import Path
+
 class Lipo(BaseDataset):
     url = 'https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/Lipophilicity.csv'
 
-    def __init__(self, csv: str|None = None):
+    def __init__(self, root: str|None = None, compression: bool = True):
+        suffix = 'csv' if compression else 'csv.gz'
+        csv = Path(root) / f'Lipophilicity.{suffix}' if root else None
         super(Lipo, self).__init__(csv=csv, url=self.url)
+
+        self.rename(columns={'smiles': 'SMILES', 'exp': 'y'}, inplace=True)
+        self.drop(
+            self.columns.difference(['SMILES', 'y']),
+            axis=1, inplace=True
+        )
+        self.save()
