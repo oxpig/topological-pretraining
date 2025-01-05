94d63c4fa5f2129918818ac6a2042bf5290f53a7
smkyrle <smoneykyrle@gmail.com>
2025-01-05 18:37:47 +0000

    dataset loading and stratified splits for classification datasets

--- _src/preprocess.py
+++ _src/preprocess.py
@@ -1,21 +1,21 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import GroupKFold, train_test_split
+from sklearn.model_selection import GroupKFold, train_test_split, StratifiedGroupKFold
 
 from rdkit import Chem, DataStructs
 from rdkit.Chem.rdFingerprintGenerator import (
-    AdditionalOutput, FingeprintGenerator64, GetMorganGenerator
+    AdditionalOutput, GetMorganGenerator
 )
 from tqdm import tqdm
 
 from .data.datasets import BaseDataset
 from .data.utils import load_dataset, load_molecules
-from .data.mol import FPOperations, Standardizer, MorganGenerator
+from .data.mol import FPOps, Standardizer, MorganGenerator
 
 def max_tanimoto(
     fps_1: list[DataStructs.ExplicitBitVect],
     fps_2: list[DataStructs.ExplicitBitVect],
-    verbose: bool = True,
+    verbose: bool = False,
     ) -> np.ndarray:
     """
     Calculate the maximum Tanimoto similarity for each molecule in fps_1 to all molecules in fps_2.
@@ -33,7 +33,7 @@ def max_tanimoto(
     out = np.zeros((len(fps_1)))
     pbar = tqdm(total=len(fps_1), disable=not verbose)
     for i, fp_1 in enumerate(fps_1):
-        sims = FPOperations.bulk_tanimoto(fp_1, fps_2)
+        sims = FPOps.bulk_tanimoto(fp_1, fps_2)
         out[i] = np.max(sims)
         pbar.update(1)
     pbar.close()
@@ -52,7 +52,8 @@ def float_to_binary(
 def tanimoto_filter(
     fp_1: list[DataStructs.ExplicitBitVect],
     fp_2: list[DataStructs.ExplicitBitVect],
-    threshold: float = 0.5
+    threshold: float = 0.5,
+    verbose: bool = False
 ) -> np.ndarray:
     """
     Get fingerprint filter for fp_1 based on Tanimoto similarity to fp_2.
@@ -67,13 +68,14 @@ def tanimoto_filter(
         The threshold for Tanimoto similarity. Labels values below the threshold with 1 and values
         above with 0. Default is 0.5.
     """
-    out = max_tanimoto(fp_1, fp_2)
+    out = max_tanimoto(fp_1, fp_2, verbose=verbose)
     return float_to_binary(out, threshold=threshold, below=True)
 
 def batch_tanimoto_filter(
     fp_1: list[DataStructs.ExplicitBitVect],
     fp_2: list[list[DataStructs.ExplicitBitVect]],
-    threshold: float = 0.5
+    threshold: float = 0.5,
+    verbose: bool = False,
 ) -> np.ndarray:
     """
     Apply Tanimoto filter to multiple sets of fingerprints.
@@ -100,26 +102,33 @@ def batch_tanimoto_filter(
 
     """
     out = np.zeros((len(fp_1), len(fp_2)+1))
+    pbar = tqdm(fp_2, disable=not verbose, desc='Benchmark dataset')
     for i, fps in enumerate(fp_2):
-        out[:, i] = tanimoto_filter(fp_1, fps, threshold=threshold)
+        out[:, i] = tanimoto_filter(fp_1, fps, threshold=threshold, verbose=verbose)
+        pbar.update(1)
+    pbar.close()
     out[:, -1] = np.where(np.sum(out, axis=1) > 0, 1, 0)
     return out
 
 def repeat_groupkfold(
-    data: np.ndarray,
-    groups: np.ndarray,
+    X: np.ndarray,
+    y: np.ndarray,
+    groups: np.ndarray|None = None,
     kfolds: int = 5,
     repeats: int = 1,
-    verbose: bool = True
+    verbose: bool = True,
+    stratified: bool = False,
 ):
     """
     Repeat GroupKFold splits.
 
     Parameters
     ----------
-    data: np.ndarray
-        The data to split.
-    groups: np.ndarray
+    X: np.ndarray
+        data to split.
+    y: np.ndarry
+        data labels to split.
+    groups: 
         The groups to split the data.
     kfolds: int
         The number of folds to split the data into.
@@ -127,6 +136,9 @@ def repeat_groupkfold(
     repeats: int
         The number of times to repeat the splits.
         Default is 1.
+    stratified: bool
+        Whether to stratify splits by y. Only applicable
+        for classification.
 
     Returns
     -------
@@ -135,14 +147,15 @@ def repeat_groupkfold(
         train set. Each row represents a data point. Total number of splits is kfolds * repeats.
     """
     total_splits = kfolds * repeats
-    out = np.full((data.shape[0], total_splits), fill_value='Train')
+    out = np.full((len(X), total_splits), fill_value='Train')
     pbar = tqdm(
         total=total_splits, disable=not verbose, desc='Generating splits'
     )
+    gkf = StratifiedGroupKFold if stratified else GroupKFold
     for i in range(repeats):
-        gkf = GroupKFold(n_splits=kfolds, shuffle=True, random_state=i)
+        splitter = gkf(n_splits=kfolds, shuffle=True, random_state=i)
         for j, (train_index, test_index) in enumerate(
-            gkf.split(data, groups=groups)
+            splitter.split(X, y, groups=groups)
         ):
             out[test_index, i * kfolds + j] = 'Test'
             pbar.update(1)
@@ -150,8 +163,9 @@ def repeat_groupkfold(
     return out
 
 def butina_splitting(
-    fps: list[DataStructs.ExplicitBitVect], threshold: float = 0.65,
-    repeats: int = 1, kfolds: int = 5, verbose: bool = True
+    fps: list[DataStructs.ExplicitBitVect], y: np.ndarray = None, threshold: float = 0.65,
+    repeats: int = 1, kfolds: int = 5, verbose: bool = True,
+    stratified = False
 ) -> np.ndarray:
     """
     Split the data using Butina clustering and GroupKFold.
@@ -169,6 +183,9 @@ def butina_splitting(
     kfolds: int
         The number of folds to split the data into.
         Default is 5.
+    stratified: bool
+        Whether to stratify splitting by y. Only applicable for classification.
+        Default is False.
 
     Returns
     -------
@@ -176,8 +193,13 @@ def butina_splitting(
         Array of splits. Each column represents a split, where 1 is the test set and 0 is the
         train set. Each row represents a data point. Total number of splits is kfolds * repeats.
     """
-    clusters = FPOperations.butina(fps, threshold=threshold, verbose=verbose)
-    return repeat_groupkfold(fps, clusters, kfolds=kfolds, repeats=repeats, verbose=verbose)
+    clusters = FPOps.butina(fps, threshold=threshold, verbose=verbose)
+    splits = repeat_groupkfold(
+        fps, y=y, groups=clusters,
+        kfolds=kfolds, repeats=repeats, verbose=verbose,
+        stratified=stratified
+    )
+    return splits, clusters
 
 def subset_indices(total: int, n: int) -> np.ndarray:
     """
@@ -228,67 +250,60 @@ def preprocess(config: dict):
     """
     Preprocess the data.
     """
-    pretrain_data = config['pretrain']
     data_path = config['data']
-    verbose = config['verbose']
+    verbose: bool = config['verbose']
 
-    pretrain_data = load_dataset(
+    benchmark_data = config['benchmark']
+
+    morgan_generator = MorganGenerator(verbose=verbose, **config['morgan'])
+    standardizer: Standardizer = Standardizer(**config['standardizer'])
+    splitter_kind = config['splitting']['kind']
+    splitter = splitters(splitter_kind)
+    splitter_params = config['splitting']['params']
+
+    benchmark_fps = {}
+
+    for benchmark in benchmark_data:
+        print(f'Processing {benchmark}') if verbose else None
+        df: BaseDataset = load_dataset(
+            benchmark, root=data_path, compression=True,
+            verbose=verbose, standardizer=standardizer
+        )
+        if 'split' in df.columns[-1]:
+            print(f'Splits already generated for {benchmark}') if verbose else None
+            continue
+        rdkit_passes = df[df['rdkit_pass'] == True]
+        mols = df.rdkit_mols
+        
+        # fps as explicitbitvect
+        morgan_generator.asarray = False
+        fps = morgan_generator(mols)
+
+        splits, groups = splitter(
+            fps, y=rdkit_passes.y.values, verbose=verbose, **splitter_params
+        )
+        if groups is not None:
+            df[f'{splitter_kind}_cluster'] = np.nan
+            df.loc[rdkit_passes.index, f'{splitter_kind}_cluster'] = groups
+        # add rows of -1 for failed molecules
+        splits = pd.DataFrame(
+            splits,
+            columns=[f'split_{i}' for i in range(splits.shape[1])],
+            index=rdkit_passes.index
+        )
+        csv_path = df.csv
+        df = df.join(splits)
+        df.to_csv(csv_path, compression='infer', index=False)
+
+        benchmark_fps[benchmark] = fps
+
+    pretrain_data = config['pretrain']
+    pretrain_data: BaseDataset = load_dataset(
         pretrain_data, root=data_path, compression=True,
         verbose=verbose
     )
-    benchmark_data = config['benchmark']
-    benchmark_data = {
-        name: load_dataset(
-            name, root=data_path, compression=True, verbose=verbose
-        ) for name in benchmark_data
-    }
-
-    morgan_generator = MorganGenerator(**config['morgan'])
-    standardizer: Standardizer = Standardizer(**config['standardizer'])
-    splitter = config['splitting']['kind']
-    splitter = splitters(splitter)
-    splitter_params = config['splitting']['params']
-    verbose: bool = True
-
-    benchmark_fps = {}
-    for key, value in benchmark_data.items():
-        print(f'Processing {key}') if verbose else None
-        mols = value['SMILES'].tolist()
-        mols = load_molecules(mols, verbose=verbose)
-        mols = [
-            standardizer(i)
-            for i in tqdm(mols, disable=not verbose, desc='Standardizing molecules')
-        ]
-        fail_and_pass = np.where(np.array(mols) != None, True, False)
-        value['pass_rdkit_standardization'] = fail_and_pass
-
-
-        # add mol checker
-
-        # fps as explicitbitvect
-        fps = [
-            morgan_generator.dense(i)
-            for i in tqdm(
-                mols, disable=not verbose, desc='Generating fingerprints'
-            ) if i is not None
-        ]
-
-        splits = splitter(
-            fps, verbose=verbose, **splitter_params
-        )
-        # add rows of -1 for failed molecules
-        splits = np.insert(splits, np.where(~fail_and_pass)[0], axis=0, values='Fail')
-        splits = pd.DataFrame(
-            splits, columns=[f'split_{i}'
-            for i in range(splits.shape[1])
-        ])
-        value.join(splits)
-        value.save()
-        benchmark_fps[key] = fps
-
-    pretrain_mols = pretrain_data['SMILES'].tolist()
-    pretrain_mols = load_molecules(pretrain_mols, verbose=verbose)
-    pretrain_mols = [standardizer(i) for i in pretrain_mols]
+    pretrain_data = pretrain_data[:10]
+    pretrain_mols = pretrain_data.rdkit_mols[:10]
     
     # fps as explicitbitvect
     pretrain_fps = [morgan_generator.dense(i) for i in pretrain_mols]
@@ -296,12 +311,14 @@ def preprocess(config: dict):
     pretrain_filter = batch_tanimoto_filter(
         pretrain_fps, benchmark_fps.values(), threshold=config['filter_threshold']
     )
+
     num_keep = np.sum(pretrain_filter[:, -1])
-    random_indices = subset_indices(pretrain_data.shape[0], num_keep)
-    random_indices = indices_to_binary(random_indices, pretrain_data.shape[0])
+
+    random_indices = subset_indices(len(pretrain_data), num_keep)
+    random_indices = indices_to_binary(random_indices, len(pretrain_data))
     pretrain_filter = np.concatenate([pretrain_filter, ], axis=1)
     
     cols = [f'{key}_filter' for key in benchmark_fps.keys()] + ['aggregate_filter', 'random_filter']
     pretrain_filter = pd.DataFrame(pretrain_filter, columns=cols)
+    csv_path = pretrain_data.csv
     pretrain_data.join(pretrain_filter)
-    pretrain_data.save()
