e28159e76909c43be828a0b7d5b539da9a5616c6
smkyrle <smoneykyrle@gmail.com>
2025-02-11 13:10:04 +0000

    tokenizer no longer handles train test splits

--- _src/tokenizers/base.py
+++ _src/tokenizers/base.py
@@ -3,7 +3,7 @@ from rdkit import Chem
 import torch
 import torch_geometric as pyg
 from tqdm import tqdm
-from typing import Callable
+from typing import Callable, Optional
 from sklearn.feature_selection import (
     SelectKBest, VarianceThreshold,
     mutual_info_classif, mutual_info_regression
@@ -22,27 +22,42 @@ class BaseTokenizer:
     cocorr = SelectAll()
     select_k_best = SelectAll()
     scaler = SelectAll()
-    use_scaler = False
+    fixed_transform_kwargs = {}
 
     def __init__(
         self,
-        X: list[Chem.Mol],
-        y: np.ndarray = None,
-        train: np.ndarray = np.array([]),
-        test: np.ndarray = np.array([]),
         transform_kwargs: dict = {},
+        variance_threshold: Optional[float] = None,
+        cocorr_threshold: Optional[float] = None,
+        k_best_task: Optional[int] = None,
+        scaler: bool = False,
         verbose: bool = False,
     ):
-        if len(train) == 0:
-            train = np.arange(len(X))
-        self.origin_X = X
         self.verbose = verbose
+        if isinstance(variance_threshold, float):
+            self.variance_threshold = VarianceThreshold(threshold=variance_threshold)
+        if isinstance(cocorr_threshold, float):
+            self.cocorr = CoCorr(threshold=cocorr_threshold)
+        if k_best_task == 'classification':
+            self.k_best_func = mutual_info_classif
+        elif k_best_task == 'regression':
+            self.k_best_func = mutual_info_regression
+        else:
+            self.k_best_func = None
+
+        if scaler:
+            self.scaler = MinMaxScaler()
+        transform_kwargs.update(self.fixed_transform_kwargs)
         self.set_transform(kwargs=transform_kwargs)
-        self.X = self.transform(X)
-        self.y = y
-        self.train_idx = train
-        self.test_idx = test
-        self.transform_kwargs = transform_kwargs
+
+
+    def __call__(self, X: Chem.Mol|list[Chem.Mol]) -> np.ndarray:
+        X = self.transform(X)
+        X = self.variance_threshold.transform(X)
+        X = self.cocorr.transform(X)
+        X = self.select_k_best.transform(X)
+        X = self.scaler.transform(X)
+        return X
 
     @property
     def transform(self):
@@ -54,142 +69,8 @@ class BaseTokenizer:
     def _transform_base(self, **kwargs):
         raise NotImplementedError
 
-    def reset(self, train: np.ndarray, test: np.ndarray) -> None:
-        self.train_idx = train
-        self.test_idx = test
-        self.variance_threshold = SelectAll()
-        self.cocorr = SelectAll()
-        self.select_k_best = SelectAll()
-        self.scaler = SelectAll()
-
-    @property
-    def train_X(self):
-        train_X = deepcopy(self.X[self.train_idx])
-        train_X = self.variance_threshold.transform(train_X)
-        train_X = self.cocorr.transform(train_X)
-        train_X = self.select_k_best.transform(train_X)
-        train_X = self.scaler.transform(train_X)
-        return train_X
-    
-    @property
-    def train_y(self):
-        return self.y[self.train_idx] if self.y is not None else None
-    
-    @property
-    def test_X(self):
-        test_X = deepcopy(self.X[self.test_idx])
-        test_X = self.variance_threshold.transform(test_X)
-        test_X = self.cocorr.transform(test_X)
-        test_X = self.select_k_best.transform(test_X)
-        test_X = self.scaler.transform(test_X)
-        return test_X
-
-    @property
-    def test_y(self):
-        return self.y[self.test_idx] if self.y is not None else None
-
-    @property
-    def train(self):
-        return self.train_X, self.train_y
-    
-    @property
-    def test(self):
-        return self.test_X, self.test_y
-        
-    def set_variance_threshold(self, threshold: float = 0.0) -> np.ndarray:
-        """
-        Remove features with low variance.
-        
-        Parameters
-        ----------
-        threshold : float
-            Variance threshold.
-        
-        Returns
-        -------
-        np.ndarray
-            Mask of features to keep.
-        """
-        self.variance_threshold = SelectAll()
-        self.cocorr = SelectAll()
-        self.select_k_best = SelectAll()
-        X_train = self.train_X
-        
-        vt = VarianceThreshold(threshold=threshold)
-        vt.fit(X_train)
-        self.variance_threshold = vt
-
-    def set_cocorr(self, threshold: float = 0.9):
-        """
-        Remove features with high cocorrelation.
-        Finds pairs of features with a correlation higher than the threshold.
-        The feature with the highest variance is kept.
-        
-        Parameters
-        ----------
-        threshold : float
-            Correlation threshold.
-        """
-        self.cocorr = SelectAll()
-        self.select_k_best = SelectAll()
-        X_train = self.train_X
-
-        cc = CoCorr(threshold=threshold)
-        cc.fit(X_train)
-        self.cocorr = cc
-
-    def set_select_k_best(self, k: int = 10, task: str = 'classification'):
-        """
-        Select the k best features.
-        
-        Parameters
-        ----------
-        score_func : Callable
-            The scoring function.
-        k : int
-            The number of features to keep.
-        """
-        self.select_k_best = SelectAll()
-        X_train, y_train = self.train
-        if y_train is None:
-            if self.verbose:
-                print('No y values for SelectKBest')
-            return SelectAll()
-        if task == 'classification':
-            score_func = mutual_info_classif
-        elif task == 'regression':
-            score_func = mutual_info_regression
-
-        skb = SelectKBest(score_func=score_func, k=k)
-        skb.fit(X_train, y_train)
-        self.select_k_best = skb
-
-    def set_min_max_scale(self, use: bool = True):
-        """
-        Min-max scale the features.
-        """
-        if use:
-            X_train = self.train_X
-            scaler = MinMaxScaler()
-            scaler.fit(X_train)
-            self.scaler = scaler
-        else:
-            self.scaler = SelectAll()
-    
-class TestTokenizer(BaseTokenizer):
-
-    def __init__(
-        self,
-        X,
-        y = None,
-        train = np.array([]),
-        test = np.array([]), 
-        transform_kwargs = {'descriptors': ['BalabanJ']}
-    ):
-        super().__init__(X, y, train, test, transform_kwargs)
-
-    def _transform_base(self, **kwargs):
-        return MolDesc(**kwargs)
+    def reset(self, mols: Chem.Mol) -> None:
+        raise NotImplementedError
     
 class BaseGraph:
 
@@ -202,38 +83,27 @@ class BaseGraph:
     def __call__(self, X: Chem.Mol|list[Chem.Mol]):
 
         if isinstance(X, Chem.Mol):
-            X = [X]
+            return self.make_graph(X)
 
-        out = {}
+        out = []
         pbar = tqdm(total=len(X), desc='Generating graphs', disable=not self.verbose)
         for idx, mol in enumerate(X):
             if not isinstance(mol, Chem.Mol):
-                out[idx] = None
+                out.append(None)
             else:
-                out[idx] = self.make_graph(mol)
+                out.append(self.make_graph(mol))
             pbar.update(1)
         pbar.close()
         return out
-    
 
 class BaseGraphTokenizer(BaseTokenizer):
 
-    edge_attr_variance_threshold = SelectAll()
-    edge_attr_cocorr = SelectAll()
-    edge_attr_scaler = SelectAll()
-    use_scaler = False
-
     def __init__(
         self,
-        X: list[Chem.Mol],
-        y: np.ndarray = None,
-        train: np.ndarray = np.array([]),
-        test: np.ndarray = np.array([]),
         transform_kwargs: dict = {},
         verbose: bool = False,
     ):
         super(BaseGraphTokenizer, self).__init__(
-            X=X, y=y, train=train, test=test,
             transform_kwargs=transform_kwargs, verbose=verbose
         )
 
