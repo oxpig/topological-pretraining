f0ad86c34bfbd5de9d70b30ffb4cf78c7b323dda
smkyrle <smoneykyrle@gmail.com>
2025-02-14 12:30:14 +0000

    tokenizer loading to and from dict

---
+++ _src/tokenizers/load.py
@@ -0,0 +1,38 @@
+from . import (
+    BaseTokenizer,
+    AtomGraphTokenizer, MorganGraphTokenizer,
+    ECFP, FCFP, PDV, SNS
+)
+import torch
+
+all_tokenizers = {
+    'AtomGraphTokenizer': AtomGraphTokenizer,
+    'MorganGraphTokenizer': MorganGraphTokenizer,
+    'ECFP': ECFP,
+    'FCFP': FCFP,
+    'PDV': PDV,
+    'SNS': SNS
+}
+
+
+def read_from_dict(parameters: dict) -> BaseTokenizer:
+    """
+    Load tokenizer from a dictionary
+    """
+    if 'name' not in parameters:
+        raise ValueError('Tokenizer name not found in parameters.')
+    tokenizer = parameters['name']
+    tokenizer = all_tokenizers[tokenizer](**parameters)
+    tokenizer.fitted = parameters['fitted']
+    return tokenizer
+
+
+def load_tokenizer(path: str, parameters: bool = True) -> BaseTokenizer:
+    """
+    Load a tokenizer from a file.
+    """
+    tokenizer = torch.load(path, weights_only=parameters)
+    if parameters:
+        return read_from_dict(tokenizer)
+    else:
+        return tokenizer
