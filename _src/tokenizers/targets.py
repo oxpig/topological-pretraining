2ddb1c56a5e800969946c98357460b2d93ca18be
smkyrle <smoneykyrle@gmail.com>
2025-02-18 10:04:50 +0000

    class for getting graph targets

---
+++ _src/tokenizers/targets.py
@@ -0,0 +1,138 @@
+from . import PDV, SNS, ECFP, FCFP
+from ..nn import pred_head
+from .. import tokenizers 
+
+from rdkit import Chem
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import VarianceThreshold
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.base import BaseEstimator, TransformerMixin
+import torch_geometric as pyg
+import torch
+
+
+
+
+
+possible_targets_ = {
+    'SNS': {
+        'head_type': 'binary',
+        'target_class': SNS,
+        'refit': False,
+        'level': 'global',
+        'input_type': 'molecule',
+        'split_dependent': True,
+        'run_dependent': False,
+    },
+    'ECFP': {
+        'head_type': 'binary',
+        'target_class': ECFP,
+        'processing': [],
+        'refit': False,
+        'level': 'global',
+        'input_type': 'molecule',
+        'split_dependent': False,
+        'run_dependent': False,
+    },
+    'FCFP': {
+        'head_type': 'binary',
+        'target_class': FCFP,
+        'level': 'global',
+        'input_type': 'molecule',
+        'split_dependent': False,
+        'run_dependent': False,
+    },
+    'PDV': {
+        'head_type': 'regression',
+        'target_class': PDV,
+        'level': 'global',
+        'input_type': 'molecule',
+        'split_dependent': False,
+        'run_dependent': False,
+    },
+}
+
+class Targets(dict):
+
+    def __init__(
+        self, targets: dict[str, dict[str, str]] = {}, targets_path: str = None
+    ):
+        super(Targets, self).__init__(**targets)
+        self.targets_path = targets_path
+        if self.targets_path is not None:
+            self.load()
+        else:
+            for target_name, target_kwargs in self.items():
+                if target_name not in possible_targets_:
+                    raise ValueError(f'Target {target_name} not supported.')
+                self[target_name]['pipeline'] = possible_targets_[target_name]['target_class'](**target_kwargs)
+                self[target_name]['prediction_head'] = possible_targets_[target_name]['head_type']
+                self[target_name]['level'] = possible_targets_[target_name]['level']
+                self[target_name]['input_type'] = possible_targets_[target_name]['input_type']
+
+    def fit(self, data: tuple[list[Chem.Mol], list[pyg.data.Data]]):
+
+        for target_name in self:
+            input_type = self[target_name]['input_type']
+            
+            if input_type == 'molecule':
+                self[target_name]['pipeline'].fit(data[0])
+            elif input_type == 'graph':
+                self[target_name]['pipeline'].fit(data[1])
+
+        return self
+
+    def transform(self, mol: Chem.Mol, graph: pyg.data.Data):
+
+        for target_name in self:
+            if target_name in graph:
+                continue
+            input_type = self[target_name]['input_type']
+            level = self[target_name]['level']
+
+            if input_type == 'molecule':
+                X = mol
+            else:
+                X = graph
+            y = self[target_name]['pipeline'].transform(X)
+            if level == 'global':
+                graph[target_name] = torch.tensor(y)
+            elif level == 'node':
+                y, mask = y
+                graph[target_name] = torch.tensor(y)
+                graph[f'{target_name}_mask'] = mask
+            else:
+                raise ValueError(f'Level {level} not supported.')
+
+        return graph
+    
+    def to_dict(self):
+        out = {}
+        for target in self:
+            out[target] = self[target].copy()
+            out[target]['pipeline'] = self[target]['pipeline'].to_dict()
+
+        return out 
+    
+    def save(self, targets_path: str = None):
+        if targets_path is None:
+            targets_path = self.targets_path
+        else:
+            self.targets_path = targets_path
+        torch.save(self.to_dict(), targets_path)
+
+    def load(self):
+        targets = torch.load(self.targets_path, weights_only=True)
+        for target in targets:
+            self[target] = targets[target]
+            self[target]['pipeline'] = tokenizers.read_from_dict(self[target]['pipeline'])
+            self[target]['prediction_head'] = possible_targets_[target]['head_type']
+            self[target]['level'] = possible_targets_[target]['level']
+            self[target]['input_type'] = possible_targets_[target]['input_type']
+
+    @property
+    def is_fitted_(self):
+        return all([
+            self[target_name]['pipeline'].__sklearn_is_fitted__()
+            for target_name in self
+        ])
