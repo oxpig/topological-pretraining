ed71276e4409d1d9e4c0db3f9fc3c1fc2cbdb5fc
smkyrle <smoneykyrle@gmail.com>
2025-02-26 10:52:28 +0000

    additional imports from tokenizer

--- _src/tokenizers/__init__.py
+++ _src/tokenizers/__init__.py
@@ -3,3 +3,5 @@ from .base import BaseTokenizer, GraphTokenizer
 from .baseline import ECFP, FCFP, PDV, SNS
 from .morgan_graph import MorganGraphTokenizer
 from .load import load_tokenizer, read_from_dict
+from .pretrained import PreTrainedTokenizer
+from ._get_tokenizer import get_tokenizer
