fix: units in M not nM

_src/data/datasets/chembl_affinity/drd2.py
@@ -25,7 +25,7 @@ class DRD2(BaseDataFrame):
                 },
                 inplace=True,
             )
-            self.y = self.y.apply(lambda x: -np.log10(x))
+            self.y = self.y.apply(lambda x: -np.log10(x) + 9)
             self.mol_standardize_check()
             self.save()
 