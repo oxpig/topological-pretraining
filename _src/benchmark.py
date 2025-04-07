bfa6ebb49b8928ff03fca7b7e6b16ad2d40d94c6
smkyrle <smoneykyrle@gmail.com>
2025-04-07 15:10:18 +0100

    premature exit

--- _src/benchmark.py
+++ _src/benchmark.py
@@ -429,6 +429,7 @@ def benchmark(config: dict):
             model.fit(train_X, train_y)
             end = datetime.now()
             print(f'Time taken to fit: {end - start}') if verbose else None
+            exit()
             print(f'Getting predictions...') if verbose else None
             train_pred = model.predict(train_X)
             out[idx, train] = train_pred
