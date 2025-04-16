fb405d9dfebd053e1f5a799f41f04381c1d25339
smkyrle <smoneykyrle@gmail.com>
2025-04-16 13:38:42 +0100

    empty as False for graphs not None

--- _src/tokenizers/base.py
+++ _src/tokenizers/base.py
@@ -273,7 +273,7 @@ class BaseGraph:
 
         return pyg.data.Data(
             x=x, edge_index=edge_index, edge_attr=edge_attr,
-            raw=True
+            raw=True, empty=False,
         )
     
     def encode(self, graph: pyg.data.Data):
