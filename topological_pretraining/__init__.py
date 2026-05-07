aa91f25b5696272302fc5a060eec12e95c5cf013
smkyrle <smoneykyrle@gmail.com>
2026-05-07 23:17:28 +0100

    fix: imports as modules

--- topological_pretraining/__init__.py
+++ topological_pretraining/__init__.py
@@ -1,7 +1,9 @@
 from . import data, featurization, importance, models, nn
-from .benchmark import benchmark
-from .preprocess import preprocess
-from .pretrain import pretrain
+from . import (
+    benchmark,
+    preprocess,
+    pretrain,
+)
 from .logging import Logger
 
 __all__ = [
